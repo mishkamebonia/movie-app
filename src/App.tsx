ad60680a38627e46fbc1a0b179cad9982787bdea
mishkamebonia <mishkas.mebonia@gmail.com>
2024-01-31 20:12:05 +0400

    created app

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.scss";
+
+function App() {
+  return (
+    <>
+      <h1>hello</h1>
+      <h2>hello</h2>
+      <h3>hello</h3>
+      <h4>hello</h4>
+      <h5>hello</h5>
+    </>
+  );
+}
+
+export default App;
