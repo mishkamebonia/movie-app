c938c205f5e7f0cbaf067f235854555c0f691172
mishkamebonia <mishkas.mebonia@gmail.com>
2024-02-01 01:02:02 +0400

    updated file

---
+++ src/components/Main.tsx
@@ -0,0 +1,13 @@
+import Search from "./Search";
+import TrendingSwiper from "./TrendingSwiper";
+
+const Main = () => {
+  return (
+    <main>
+      <Search />
+      <TrendingSwiper />
+    </main>
+  );
+};
+
+export default Main;
