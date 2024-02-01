6676f68979f2bf2c612ff7572f78cfa81592570a
mishkamebonia <mishkas.mebonia@gmail.com>
2024-02-01 17:18:00 +0400

    refactored file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.scss";
 import SideBar from "./components/SideBar";
-import Home from "./pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 import MoviesPage from "./pages/MoviesPage";
 import SeriesPage from "./pages/SeriesPage";
 import Bookmarked from "./pages/Bookmarked";
+import DetailsPage from "./pages/DetailsPage";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path="/" index element={<Home />} />
           <Route path="/movies" element={<MoviesPage />} />
+          <Route path="/movies/:movieId" element={<DetailsPage />} />
           <Route path="/series" element={<SeriesPage />} />
           <Route path="/bookmarked" element={<Bookmarked />} />
         </Routes>
