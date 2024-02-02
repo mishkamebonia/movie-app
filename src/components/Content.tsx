1d0c6d927dff072bf741aa252c0955475db128b4
mishkamebonia <mishkas.mebonia@gmail.com>
2024-02-02 22:01:42 +0400

    refactored content

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,13 +1,34 @@
-import Trending from "./movies/Trending";
-import Movies from "./movies/Movies";
-import Series from "./movies/Series";
+import { movieApi, seriesApi, newMoviesUrl } from "../config/movieApi";
+import Slider from "./movies/Slider";
+import "./Content.scss";
 
 const Content = () => {
   return (
-    <div>
-      <Trending />
-      <Movies />
-      <Series />
+    <div className="content">
+      <Slider
+        dataApi={movieApi}
+        title="Popular Movies"
+        url="/movies/"
+        slides="4"
+        image="backdrop"
+        autoplay={true}
+      />
+      <Slider
+        dataApi={newMoviesUrl}
+        title="Movies"
+        url="/movies/"
+        slides="6"
+        image="poster"
+        autoplay={false}
+      />
+      <Slider
+        dataApi={seriesApi}
+        title="TV Series"
+        url="/series/"
+        slides="6"
+        image="poster"
+        autoplay={false}
+      />
     </div>
   );
 };
