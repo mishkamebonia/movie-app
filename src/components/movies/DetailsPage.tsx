55b1f6239d89221628a3e16d5ae74fe02219ab38
mishkamebonia <mishkas.mebonia@gmail.com>
2024-02-02 22:02:00 +0400

    updated details page

--- src/components/movies/DetailsPage.tsx
+++ src/components/movies/DetailsPage.tsx
@@ -2,8 +2,9 @@ import { apiKey } from "../../config/movieApi";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import YouTube from "react-youtube";
-import Trending from "./Trending";
 import Search from "../Search";
+import Slider from "./Slider";
+import { movieApi } from "../../config/movieApi";
 import "./DetailsPage.scss";
 
 const DetailsPage = (props) => {
@@ -11,6 +12,7 @@ const DetailsPage = (props) => {
   const { movieId, seriesId } = useParams();
   const [link, setLink] = useState(null);
   const [trailerKey, setTrailerKey] = useState(null);
+  const [opts, setOpts] = useState({ height: "480", width: "854" });
 
   useEffect(() => {
     const id = movieId || seriesId;
@@ -37,119 +39,140 @@ const DetailsPage = (props) => {
     });
   }, [movieId, seriesId]);
 
+  useEffect(() => {
+    function handleResize() {
+      if (window.innerWidth <= 1439) {
+        setOpts({ height: "200", width: "200" });
+      } else {
+        setOpts({ height: "480", width: "854" });
+      }
+    }
+
+    window.addEventListener("resize", handleResize);
+    handleResize(); // Call on initial render
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   if (!link) {
     return <div>Loading...</div>;
   }
 
-  const opts = {
-    height: "600",
-    width: "100%",
-  };
-
   return (
     <main>
-      <Search placeholder={placeholder} />
-      <div className="details-page">
-        <div className="row">
-          <img
-            className="poster"
-            src={`https://image.tmdb.org/t/p/w500${link.poster_path}`}
-            alt=""
-          />
-          <div className="trailer">
-            {trailerKey && <YouTube videoId={trailerKey} opts={opts} />}
+      <div className="content">
+        <Search placeholder={placeholder} />
+        <div className="details-page">
+          <div className="row">
+            <img
+              className="poster"
+              src={`https://image.tmdb.org/t/p/w500${link.poster_path}`}
+              alt=""
+            />
+            <div className="trailer">
+              {trailerKey && <YouTube videoId={trailerKey} opts={opts} />}
+            </div>
           </div>
-        </div>
-        <div>
-          <h1>{link.title || link.original_name}</h1>
-          {seriesId ? (
+          <div>
+            <h1>{link.title || link.name}</h1>
+            {seriesId ? (
+              <p>
+                <span>Created by: </span>
+                {link.created_by.map((author) => (
+                  <a href="#" key={author.id} style={{ marginRight: "8px" }}>
+                    {author.name},
+                  </a>
+                ))}
+              </p>
+            ) : null}
             <p>
-              <span>Created by: </span>
-              {link.created_by.map((author) => (
-                <a href="#" key={author.id} style={{ marginRight: "8px" }}>
-                  {author.name},
+              <span>Genres:</span>
+              {link.genres.map((genre) => (
+                <a href="#" key={genre.id} style={{ marginRight: "8px" }}>
+                  {genre.name},
                 </a>
               ))}
             </p>
-          ) : null}
-          <p>
-            <span>Genres:</span>
-            {link.genres.map((genre) => (
-              <a href="#" key={genre.id} style={{ marginRight: "8px" }}>
-                {genre.name},
+            <p>
+              <span>Studio:</span>
+              {link.production_companies.map((companie) => (
+                <a href="#" key={companie.id} style={{ marginRight: "8px" }}>
+                  {companie.name},
+                </a>
+              ))}
+            </p>
+            <p>
+              <span>Released date:</span>
+              <a href="#">
+                {new Date(link.release_date).getFullYear() |
+                  new Date(link.first_air_date).getFullYear()}
               </a>
-            ))}
-          </p>
-          <p>
-            <span>Studio:</span>
-            {link.production_companies.map((companie) => (
-              <a href="#" key={companie.id} style={{ marginRight: "8px" }}>
-                {companie.name},
-              </a>
-            ))}
-          </p>
-          <p>
-            <span>Released date:</span>
-            <a href="#">
-              {new Date(link.release_date).getFullYear() |
-                new Date(link.first_air_date).getFullYear()}
-            </a>
-          </p>
-          <p>
+            </p>
+            <p>
+              {movieId ? (
+                <p>
+                  <span>Duration:</span>
+                  {link.runtime} min
+                </p>
+              ) : null}
+            </p>
+            <p>
+              <span>Language:</span>
+              {link.spoken_languages.map((lang) => (
+                <a href="#" key={lang.id} style={{ marginRight: "8px" }}>
+                  {lang.english_name},
+                </a>
+              ))}
+            </p>
+            <p>
+              <span>Country:</span>
+              {link.production_countries.map((country) => (
+                <a href="#" key={country.id} style={{ marginRight: "8px" }}>
+                  {country.name},
+                </a>
+              ))}
+            </p>
             {movieId ? (
-              <p>
-                <span>Duration:</span>
-                {link.runtime} min
-              </p>
+              <>
+                <p>
+                  <span>Budget:</span>
+                  {link.budget}
+                </p>
+                <p>
+                  <span>Revenue:</span>
+                  {link.revenue}
+                </p>
+              </>
             ) : null}
-          </p>
-          <p>
-            <span>Language:</span>
-            {link.spoken_languages.map((lang) => (
-              <a href="#" key={lang.id} style={{ marginRight: "8px" }}>
-                {lang.english_name},
-              </a>
-            ))}
-          </p>
-          <p>
-            <span>Country:</span>
-            {link.production_countries.map((country) => (
-              <a href="#" key={country.id} style={{ marginRight: "8px" }}>
-                {country.name},
-              </a>
-            ))}
-          </p>
-          {movieId ? (
-            <>
-              <p>
-                <span>Budget:</span>
-                {link.budget}
-              </p>
-              <p>
-                <span>Revenue:</span>
-                {link.revenue}
-              </p>
-            </>
-          ) : null}
-          {seriesId ? (
-            <>
-              <p>
-                <span>Episodes:</span>
-                {link.number_of_episodes}
-              </p>
-              <p>
-                <span>Seasones:</span>
-                {link.number_of_seasons}
-              </p>
-            </>
-          ) : null}
-          <div className="description">
-            <h4>The story:</h4>
-            <p>{link.overview}</p>
+            {seriesId ? (
+              <>
+                <p>
+                  <span>Episodes:</span>
+                  {link.number_of_episodes}
+                </p>
+                <p>
+                  <span>Seasones:</span>
+                  {link.number_of_seasons}
+                </p>
+              </>
+            ) : null}
+            <div className="description">
+              <h4>The story:</h4>
+              <p>{link.overview}</p>
+            </div>
           </div>
         </div>
       </div>
-      <Trending />
+      <Slider
+        dataApi={movieApi}
+        title="Popular Movies"
+        url="/movies/"
+        slides="4"
+        image="backdrop"
+        autoplay={true}
+      />
     </main>
   );
 };
