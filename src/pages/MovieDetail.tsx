55fa3c9c1dc623632c6b9f77d6cf67669ec974d6
mishkamebonia <mishkas.mebonia@gmail.com>
2024-02-03 18:18:13 +0400

    updated details files

--- src/pages/MovieDetail.tsx
+++ src/pages/MovieDetail.tsx
@@ -1,11 +1,13 @@
 import DetailsPage from "../components/movies/DetailsPage";
 import { movieUrl, videoUrl } from "../config/movieApi";
+import { routes } from "../App";
 
 const MovieDetail = () => {
   return (
     <DetailsPage
       url={movieUrl}
       videoUrl={videoUrl}
+      route={routes.movies}
       placeholder="Search for movies"
     />
   );
--- src/pages/SerieDetail.tsx
+++ src/pages/SerieDetail.tsx
@@ -1,11 +1,13 @@
 import DetailsPage from "../components/movies/DetailsPage";
 import { SerieUrl, videoUrl } from "../config/movieApi";
+import { routes } from "../App";
 
 const SerieDetail = () => {
   return (
     <DetailsPage
       url={SerieUrl}
       videoUrl={videoUrl}
+      route={routes.series}
       placeholder="Search for TV series"
     />
   );
