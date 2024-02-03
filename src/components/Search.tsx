dec32eb0ce2ef2997bb9c7202fa827f9d95b5ed2
mishkamebonia <mishkas.mebonia@gmail.com>
2024-02-03 18:16:45 +0400

    refactored serch

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,39 +1,26 @@
 import "./Search.scss";
-import { useState } from "react";
 
 const Search = (props) => {
-  const { setSearchList, searchApi, placeholder } = props;
-  const [query, setQuery] = useState("");
-
-  const search = async (e) => {
-    e.preventDefault();
-
-    try {
-      const url = `${searchApi}&query=${query}`;
-      const res = await fetch(url);
-      const data = await res.json();
-      setSearchList(data.results);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const changeHandler = (e) => {
-    setQuery(e.target.value);
-  };
+  const { onSearch, placeholder } = props;
 
   return (
     <div className="search-input">
       <label htmlFor="search">
         <i className="fa-solid fa-magnifying-glass"></i>
       </label>
-      <form onSubmit={search}>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          const formData = new FormData(event.currentTarget);
+
+          onSearch(formData.get("search"));
+        }}
+      >
         <input
           id="search"
           placeholder={placeholder}
           type="search"
-          value={query}
-          onChange={changeHandler}
+          name="search"
         />
       </form>
     </div>
