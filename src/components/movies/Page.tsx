fbeb0fcccb368855043b08a659c3cede7f5bb36d
mishkamebonia <mishkas.mebonia@gmail.com>
2024-02-04 17:58:34 +0400

    added loader on page

--- src/components/movies/Page.tsx
+++ src/components/movies/Page.tsx
@@ -3,29 +3,34 @@ import { useNavigate, useLocation, Link } from "react-router-dom";
 import Search from "../Search";
 import "../../components/movies/movie-cards.scss";
 import { Pagination } from "@mui/material";
+import Loader from "../Loader";
+import { Skeleton } from "@mui/material";
 
 const Page = (props) => {
   const { title, apiUrl, apiSearch, placeholder, pageUrl } = props;
 
   const [movieList, setMovieList] = useState([]);
   const [totalPages, setTotalPages] = useState(1);
+  const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
   const pageNumber = parseInt(searchParams.get("page") || "1");
   const query = searchParams.get("query") || "";
 
-  console.log("rerendered");
-
   const getMovie = () => {
-    console.log("fetched");
-
     const url = query ? apiSearch : apiUrl;
+    setLoading(true);
     fetch(`${url}&page=${pageNumber}&query=${query}`)
       .then((res) => res.json())
       .then((json) => {
         setMovieList(json.results);
         setTotalPages(json.total_pages);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setLoading(false);
       });
   };
 
@@ -42,6 +47,11 @@ const Page = (props) => {
     });
   };
 
+  // if (loading) {
+  //   return <Loader />;
+  // }
+  console.log(loading);
+
   return (
     <main>
       <Search
@@ -55,25 +65,34 @@ const Page = (props) => {
           <h1>{title}</h1>
           <div className="movies-row">
             {movieList &&
-              movieList.map((movie) => (
-                <Link
-                  to={`${pageUrl}${movie.id}`}
-                  key={movie.id}
-                  className="movies"
-                >
-                  <img
-                    src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
-                    alt=""
+              movieList.map((movie) =>
+                loading ? (
+                  <Skeleton
+                    sx={{ bgcolor: "grey.900" }}
+                    variant="rectangular"
+                    width="100%"
+                    height="233px"
                   />
-                  <div className="row">
-                    <p className="title">{movie.title || movie.name}</p>
-                    <p className="light-text date">
-                      {new Date(movie.release_date).getFullYear() ||
-                        new Date(movie.first_air_date).getFullYear()}
-                    </p>
-                  </div>
-                </Link>
-              ))}
+                ) : (
+                  <Link
+                    to={`${pageUrl}${movie.id}`}
+                    key={movie.id}
+                    className="movies"
+                  >
+                    <img
+                      src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+                      alt=""
+                    />
+                    <div className="row">
+                      <p className="title">{movie.title || movie.name}</p>
+                      <p className="light-text date">
+                        {new Date(movie.release_date).getFullYear() ||
+                          new Date(movie.first_air_date).getFullYear()}
+                      </p>
+                    </div>
+                  </Link>
+                )
+              )}
           </div>
           <Pagination
             className="pagitation"
