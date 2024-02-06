c2a0912222c23bc38abf9e96ea181f88b92af9bc
mishkamebonia <mishkas.mebonia@gmail.com>
2024-02-06 23:43:54 +0400

    created auth

---
+++ src/providers/auth.tsx
@@ -0,0 +1,85 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { auth } from "../config/firebase";
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signOut,
+  User,
+} from "firebase/auth";
+
+interface AuthContextType {
+  user: null | User;
+  signUp: (email: string, password: string) => void;
+  signIn: (email: string, password: string) => void;
+  logOut: VoidFunction;
+}
+const AuthContext = createContext<AuthContextType | null>(null);
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export function AuthContextProvider({ children }: Props) {
+  const [user, setUser] = useState<User | null>(null);
+  console.log(user);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user: User | null) => {
+      setUser(user);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const signIn = (email, password) => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        setUser(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode);
+        console.log(errorMessage);
+      });
+  };
+
+  const signUp = (email, password) => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        setUser(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode);
+        console.log(errorMessage);
+      });
+  };
+
+  const logOut = () => {
+    signOut(auth)
+      .then(() => {})
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, signIn, signUp, logOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuthContext() {
+  const ctx = useContext(AuthContext);
+
+  if (!ctx) {
+    throw "useAuthContext must be called within AuthContextProvider";
+  }
+
+  return ctx;
+}
