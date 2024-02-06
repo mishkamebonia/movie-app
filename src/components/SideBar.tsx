5d309dcc318068e4d4313a3d8aa0b760af9836bf
mishkamebonia <mishkas.mebonia@gmail.com>
2024-02-07 03:45:47 +0400

    refactored files and created user button

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -4,6 +4,10 @@ import main from "../assets/main.svg";
 import film from "../assets/film.svg";
 import tv from "../assets/tv.svg";
 import marked from "../assets/marked.svg";
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 import "./SideBar.scss";
 import { useAuthContext } from "../providers/auth";
 import { routes } from "../App";
@@ -13,6 +17,15 @@ const SideBar = () => {
 
   const navigate = useNavigate();
 
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   const handleLogOut = () => {
     logOut();
     navigate(routes.login);
@@ -21,32 +34,50 @@ const SideBar = () => {
   return (
     <nav className="side-bar">
       <div className="menu">
-        <NavLink to="/">
+        <NavLink to={routes.home}>
           <img src={logo} alt="logo" />
         </NavLink>
         <div className="btn-row">
-          <NavLink to="/">
+          <NavLink to={routes.home}>
             <img src={main} alt="main" />
           </NavLink>
-          <NavLink to="/movies">
+          <NavLink to={routes.movies}>
             <img src={film} alt="film" />
           </NavLink>
-          <NavLink to="/series">
+          <NavLink to={routes.series}>
             <img src={tv} alt="tv" />
           </NavLink>
-          <NavLink to="/bookmarked">
+          <NavLink to={routes.bookmarked}>
             <img src={marked} alt="marked" />
           </NavLink>
         </div>
       </div>
 
       <div className="user">
-        <button>
-          <i className="fa-solid fa-circle-user"></i>
-        </button>
-        <button style={{ color: "white" }} onClick={() => handleLogOut()}>
-          log out
-        </button>
+        <div>
+          <Button
+            id="basic-button"
+            aria-controls={open ? "basic-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+            onClick={handleClick}
+          >
+            <i className="fa-solid fa-circle-user"></i>
+          </Button>
+          <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              "aria-labelledby": "basic-button",
+            }}
+          >
+            <MenuItem onClick={handleClose}>Profile</MenuItem>
+            <MenuItem onClick={handleClose}>My account</MenuItem>
+            <MenuItem onClick={handleLogOut}>Logout</MenuItem>
+          </Menu>
+        </div>
       </div>
     </nav>
   );
