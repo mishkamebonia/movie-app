5b856954b2349307291a1e427dbba253be41d612
mishkamebonia <mishkas.mebonia@gmail.com>
2024-02-07 03:46:20 +0400

    updated login component

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,110 +1,59 @@
-import * as React from "react";
-import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { NavLink, useNavigate } from "react-router-dom";
 import { routes } from "../App";
 import { useAuthContext } from "../providers/auth";
 import { useState } from "react";
-
-const defaultTheme = createTheme();
+import logo from "../assets/logo.svg";
+import "../scss/form.scss";
 
 const Login = () => {
-  const { signIn } = useAuthContext();
+  const { user, signIn } = useAuthContext();
 
-  const [signInEmail, setSignInEmail] = useState<string>("");
-  const [signInPassword, setSignInPassword] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
 
   const navigate = useNavigate();
+  console.log(user);
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    signIn(signInEmail, signInPassword);
-    navigate(routes.home);
+    signIn(email, password);
   };
 
+  if (user) {
+    return navigate(routes.home);
+  }
+
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Typography component="h1" variant="h5">
-            Login
-          </Typography>
-          <Box
-            component="form"
-            onSubmit={handleSubmit}
-            noValidate
-            sx={{ mt: 1 }}
-          >
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              value={signInEmail}
-              onChange={(e) => setSignInEmail(e.target.value)}
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              value={signInPassword}
-              onChange={(e) => setSignInPassword(e.target.value)}
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Login
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <NavLink to={routes.signUp} variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </NavLink>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-      </Container>
-    </ThemeProvider>
+    <div>
+      <div className="form-container">
+        <img src={logo} alt="logo" />
+        <form className="form" onSubmit={handleSubmit}>
+          <h1>Login</h1>
+          <input
+            className="input"
+            type="email"
+            placeholder="Email address"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            className="input"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button className="button">Login to your account</button>
+          <p>
+            Don’t have an account?
+            <NavLink to={routes.signUp} className="link">
+              Sign Up
+            </NavLink>
+          </p>
+        </form>
+      </div>
+    </div>
   );
 };
 
