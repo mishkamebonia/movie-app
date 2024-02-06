5a10a0989d2ebe49c7a2b067856ef637ad094c0b
mishkamebonia <mishkas.mebonia@gmail.com>
2024-02-06 23:44:42 +0400

    added navigate

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,12 +1,23 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import logo from "../assets/logo.svg";
 import main from "../assets/main.svg";
 import film from "../assets/film.svg";
 import tv from "../assets/tv.svg";
 import marked from "../assets/marked.svg";
 import "./SideBar.scss";
+import { useAuthContext } from "../providers/auth";
+import { routes } from "../App";
 
 const SideBar = () => {
+  const { logOut } = useAuthContext();
+
+  const navigate = useNavigate();
+
+  const handleLogOut = () => {
+    logOut();
+    navigate(routes.login);
+  };
+
   return (
     <nav className="side-bar">
       <div className="menu">
@@ -32,6 +43,9 @@ const SideBar = () => {
       <div className="user">
         <button>
           <i className="fa-solid fa-circle-user"></i>
+        </button>
+        <button style={{ color: "white" }} onClick={() => handleLogOut()}>
+          log out
         </button>
       </div>
     </nav>
