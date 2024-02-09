6b5f09d1fc5afc72b1c615d72c737d243b39d9c5
mishkamebonia <mishkas.mebonia@gmail.com>
2024-02-09 22:32:34 +0400

    added search logic

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,61 @@
-import Search from "../components/Search";
+import * as React from "react";
 import Content from "../components/Content";
 import { searchMovieAndSeriesApi } from "../config/movieApi";
 import Loader from "../components/Loader";
+import { Button, Backdrop } from "@mui/material";
+import "../scss/search.scss";
+import "../scss/input.scss";
 
 const Home = () => {
+  const [datas, setDatas] = React.useState([]);
+  const [query, setQuery] = React.useState("");
+  const [open, setOpen] = React.useState(false);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    fetch(`${searchMovieAndSeriesApi}?query=${query}`)
+      .then((res) => res.json())
+      .then((json) => setDatas(json.results));
+  };
+
   return (
     <main>
-      <Search
-        placeholder="Search for movies or TV series"
-        searchApi={searchMovieAndSeriesApi}
-      />
+      <div className="search-input">
+        <label htmlFor="search">
+          <i className="fa-solid fa-magnifying-glass"></i>
+        </label>
+        <form onSubmit={handleSubmit}>
+          <input
+            id="search"
+            placeholder="search"
+            type="search"
+            name="search"
+            onChange={(e) => setQuery(e.target.value)}
+            onClick={handleOpen}
+          />
+          <Backdrop
+            sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+            open={open}
+            onClick={handleClose}
+          >
+            <div className="search-module">
+              <div className="row">
+                {datas.map((data) => (
+                  <div className="card">{data.title}</div>
+                ))}
+              </div>
+            </div>
+          </Backdrop>
+        </form>
+      </div>
       <Content />
     </main>
   );
