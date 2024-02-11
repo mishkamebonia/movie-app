7f4b3ea8af83c9dee3413746f4eb1f53d9b324fc
mishkamebonia <mishkas.mebonia@gmail.com>
2024-02-11 23:14:12 +0400

    refactored

--- src/pages/Bookmarked.tsx
+++ src/pages/Bookmarked.tsx
@@ -2,41 +2,52 @@ import Search from "../components/Search";
 import { doc, getDocs, collection } from "firebase/firestore";
 import { db } from "../config/firebase";
 import { useEffect, useState } from "react";
+import { posterApi } from "../config/movieApi";
+import { Link } from "react-router-dom";
+import { useAuthContext } from "../providers/auth";
 
 const Bookmarked = () => {
+  const { user } = useAuthContext();
   const [datas, setDatas] = useState([]);
-  const bookmarkedCollectionRef = collection(db, "usersfavorites");
-
-  const getTodoList = async () => {
-    try {
-      const data = await getDocs(bookmarkedCollectionRef);
-
-      const filteredData = data.docs.map((doc) => ({
-        ...doc.data(),
-        id: doc.id,
-        movies: doc.data().movies,
-        user_id: doc.data().user_id,
-      }));
-      setDatas(filteredData);
-      console.log(filteredData);
-    } catch (err) {
-      console.error(err);
-    }
-  };
+  const bookmarkedCollectionRef = collection(db, "usersBookmarked");
 
   useEffect(() => {
-    getTodoList();
-  }, []);
+    const getTodoList = async () => {
+      try {
+        const data = await getDocs(bookmarkedCollectionRef);
+        const filteredData = data.docs
+          .filter((doc) => doc.data().uid === user.uid)
+          .map((doc) => ({
+            ...doc.data(),
+            id: doc.id,
+            movieId: doc.data().movieId,
+            imageUrl: doc.data().imageURL,
+            title: doc.data().title,
+          }));
+        setDatas(filteredData);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    if (user) {
+      getTodoList();
+    }
+  }, [user]);
 
   return (
     <main>
       <Search placeholder="Search for bookmarked shows" />
       <h1>bookmarked page</h1>
       {datas.map((data) => (
-        <div>
-          <p>moveis: {data.movies}</p>
-          <p>user id: {data.user_id}</p>
-        </div>
+        <Link to={`/movies/${data.movieId}`} key={data.id}>
+          <p>{data.imageURL}</p>
+          <img
+            src={`https://image.tmdb.org/t/p/w500/${data.imageURL}`}
+            alt=""
+          />
+          <p>User ID: {data.uid}</p>
+        </Link>
       ))}
     </main>
   );
