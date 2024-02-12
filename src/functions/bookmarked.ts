edfb803720476f10febcd3d1367762fba4819f44
mishkamebonia <mishkas.mebonia@gmail.com>
2024-02-12 20:40:16 +0400

    created bookmarked function

---
+++ src/functions/bookmarked.ts
@@ -0,0 +1,37 @@
+import { deleteDoc, doc, setDoc, getDoc } from "firebase/firestore";
+import { db } from "../config/firebase";
+
+const bookmarked = async (
+  uid,
+  type,
+  movieId,
+  title,
+  imdb,
+  date,
+  poster
+) => {
+  try {
+    const docRef = doc(db, "usersBookmarked", `${uid}_${movieId}`);
+    const docSnapshot = await getDoc(docRef);
+
+    if (docSnapshot.exists()) {
+      await deleteDoc(docRef);
+      console.log("Movie removed from bookmarks successfully!");
+    } else {
+      await setDoc(docRef, {
+        uid: uid,
+        type: type,
+        movieId: movieId,
+        title: title,
+        imdb: imdb,
+        date: date,
+        poster: poster,
+      });
+      console.log("Movie bookmarked successfully!");
+    }
+  } catch (error) {
+    console.error("Error bookmarking movie: ", error);
+  }
+};
+
+export default bookmarked
