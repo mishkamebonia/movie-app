b649551fade98b36b344906f2599e2b49476cc70
mishkamebonia <mishkas.mebonia@gmail.com>
2024-02-28 23:31:11 +0400

    added new types in movies

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -21,7 +21,9 @@ export interface Movie {
   popularity: number;
   poster_path: string;
   release_date: string;
+  first_air_date: string;
   title: string;
+  name: string;
   video: boolean;
   vote_average: number;
   vote_count: number;
