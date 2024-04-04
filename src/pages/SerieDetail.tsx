7277f73c0344228f7a67f966e6b6e3243b358206
mishkamebonia <mishkas.mebonia@gmail.com>
2024-04-04 21:49:06 +0400

    refactored

--- src/pages/SerieDetail.tsx
+++ src/pages/SerieDetail.tsx
@@ -1,10 +1,14 @@
 import DetailsPage from "../components/movies/DetailsPage";
 import { SerieUrl, videoUrl } from "../config/movieApi";
 import { routes } from "../App";
+import { useParams } from "react-router-dom";
 
 const SerieDetail = () => {
+  const { seriesId } = useParams();
+
   return (
     <DetailsPage
+      key={`series-${seriesId}`}
       type="TV Series"
       url={SerieUrl}
       videoUrl={videoUrl}
