39f22685b826ef3a4f114aef504fad0a94e6b4aa
mishkamebonia <mishkas.mebonia@gmail.com>
2024-04-04 21:51:52 +0400

    refactored

--- src/pages/MovieDetail.tsx
+++ src/pages/MovieDetail.tsx
@@ -1,10 +1,14 @@
 import DetailsPage from "../components/movies/DetailsPage";
 import { movieUrl, videoUrl } from "../config/movieApi";
 import { routes } from "../App";
+import { useParams } from "react-router-dom";
 
 const MovieDetail = () => {
+  const { movieId } = useParams();
+
   return (
     <DetailsPage
+      key={`movies-${movieId}`}
       type="Movies"
       url={movieUrl}
       videoUrl={videoUrl}
