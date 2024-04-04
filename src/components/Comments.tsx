983f9111754620ddee7f6589b04ce10d52ad8f63
mishkamebonia <mishkas.mebonia@gmail.com>
2024-04-04 21:49:20 +0400

    refactored

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,25 +1,85 @@
 import "../scss/comments.scss";
 import "../scss/images.scss";
 import { useAuthContext } from "../providers/auth";
-import { useState } from "react";
-// import { collection, docs, doc } from "firebase/firestore";
-// import { db } from "../config/firebase";
+import { useEffect, useState, useCallback } from "react";
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  addDoc,
+  orderBy,
+  serverTimestamp,
+} from "firebase/firestore";
+import { db } from "../config/firebase";
+import { create } from "@mui/material/styles/createTransitions";
+const commnetsRef = collection(db, "MoviesComments");
 
-const Comments = () => {
+type UserInfo = { id: string; name: string; image: string };
+type Comment = {
+  id: string;
+  text: string;
+  movieId: string;
+  user: UserInfo;
+};
+const Comments = (props) => {
+  const { movieId, seriesId } = props;
+
+  console.log("rerendered");
   const { user } = useAuthContext();
-  const [allComments, setAllComments] = useState([]);
+  const [allComments, setAllComments] = useState<Comment[]>([]);
   const [createComment, setCreateComment] = useState("");
+
+  const getComments = useCallback(async () => {
+    console.log({ movieId, seriesId });
+    const commentsQuery = query(
+      commnetsRef,
+      where("movieId", "==", movieId || seriesId),
+      orderBy("timestamp", "desc")
+    );
+
+    const comments: Comment[] = [];
+    const response = await getDocs(commentsQuery);
+
+    response.forEach(async (doc) => {
+      const data = doc.data();
+
+      comments.push({
+        text: data.text,
+        movieId: data.movieId,
+        id: doc.id,
+        user: {
+          id: data.userId,
+          name: data.userName,
+          image: data.userImage,
+        },
+      });
+    });
+
+    setAllComments(comments);
+    setCreateComment("");
+  }, [movieId, seriesId]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    try {
-      setAllComments([createComment, ...allComments]);
-      setCreateComment("");
-    } catch (err) {
-      console.error(err);
+    if (createComment != "") {
+      await addDoc(commnetsRef, {
+        text: createComment,
+        movieId: movieId || seriesId,
+        userId: user?.uid,
+        userName: user?.displayName,
+        userImage: user?.photoURL,
+        timestamp: serverTimestamp(),
+      });
     }
+
+    getComments();
   };
+
+  useEffect(() => {
+    getComments();
+  }, []);
 
   return (
     <div className="commentsSection">
@@ -39,11 +99,19 @@ const Comments = () => {
           <button className="button">Send</button>
         </form>
       </div>
-      <div className="all-comments">
-        {allComments.map((comment, index) => (
-          <p key={index}>{comment}</p>
-        ))}
-      </div>
+      {allComments.length !== 0 && (
+        <div className="all-comments">
+          {allComments.map((comment) => (
+            <div className="comments" key={comment.id}>
+              <div className="row">
+                <img src={comment.user.image} alt="User Avatar" />
+                <h4>{comment.user.name}</h4>
+              </div>
+              <p>{comment.text}</p>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
